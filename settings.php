3719a35f2d14da65febb651e1f95ab84edd54817
Dhiraj Patra <dhiraj.patra@gmail.com>
2017-10-24 08:20:40 +0530

    updated
    updated

--- settings.php
+++ settings.php
@@ -6,15 +6,23 @@ return [
 
         // Please replace your_* with your config for database
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=slim;charset=utf8',
-            'usr' => 'root',
-            'pwd' => 'mysqladmin'
+            'pdo1' => [
+                'dsn' => 'mysql:host=localhost;dbname=slim;charset=utf8',
+                'usr' => 'root',
+                'pwd' => 'pass'
+            ],
+            'pdo2' => [
+                'dsn' => 'mysql:host=localhost;dbname=db2;charset=utf8',
+                'usr' => 'root',
+                'pwd' => 'pass'
+            ]
+
         ],
 
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
-            'path' => __DIR__ . '/../var/log/app-'.date('Y-m').'.log',
+            'path' => __DIR__ . '/var/log/app-'.date('Y-m').'.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
     ],
