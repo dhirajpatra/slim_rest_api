c42b3fe68d8a07d2805ad642a9d67fe901650e31
Dhiraj Patra <dhiraj.patra@gmail.com>
2017-10-24 08:18:27 +0530

    updated
    updated

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,28 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// PDO object to work on
+$container['PDO1'] = function ($c) {
+    $setting = $c->get('settings')['db']['pdo1'];
+    return new \Slim\PDO\Database($setting['dsn'], $setting['usr'], $setting['pwd']);
+};
+
+$container['PDO2'] = function ($c) {
+    $setting = $c->get('settings')['db']['pdo2'];
+    return new \Slim\PDO\Database($setting['dsn'], $setting['usr'], $setting['pwd']);
+};
+
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../public/templates/', [
+        'cache' => __DIR__ . '/../var/cache/twig',
+        'cache' => false,
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
